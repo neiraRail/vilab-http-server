7cd3adf7c82efcffb60a48225913db557ffc5f5e
Rail Neira <r.neira03@ufromail.cl>
2024-04-14 22:21:27 -0400

    Implementa ejecución y stopeo de jobs

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 class Job:
-    def __init__(self, node, num_mediciones, tiempo, delay):
+    def __init__(self, node, num_mediciones, tiempo, delay, active):
         self.n = node
         self.nm = num_mediciones
         self.t = tiempo
         self.d = delay
+        self.a = active
