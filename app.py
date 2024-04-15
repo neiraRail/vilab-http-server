519f220a63db90257d6b78269104981031b7a62b
Rail Neira <r.neira03@ufromail.cl>
2024-04-14 22:21:27 -0400

    Implementa ejecución y stopeo de jobs

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_executor import Executor
 from models import Job
 from os import environ as env
 from bson import ObjectId
+import time
 
 mongohost = env.get("MONGO_HOST", "localhost")
 
 app = Flask(__name__)
+app.config['EXECUTOR_PROPAGATE_EXCEPTIONS'] = True
 app.config["MONGO_URI"] = f"mongodb://{mongohost}:27017/mydatabase"
+
+executor = Executor(app)
 mongo = PyMongo(app)
 
 # Rutas de Nodo 
@@ -37,8 +42,30 @@ def init():
     
     return jsonify(nodo)
 
+def encender_nodo(nodo_id):
+    mongo.db.nodos.find_one_and_update({"n": nodo_id}, {"$set": {"a": 1}})
+    # return jsonify(nodo)
+
+def apagar_nodo(nodo_id):
+    mongo.db.nodos.find_one_and_update({"n": nodo_id}, {"$set": {"a": 0}})
+    # return jsonify(nodo)
+
 
 # Rutas de Job
+
+def run_job(job):
+    for _ in range(job['nm']):
+        # query for "a" to check if the job is still active
+        active = mongo.db.jobs.find_one({"_id": job["_id"]}, {"a": 1, "_id": 0})["a"]
+        if active != 1:
+            break
+
+        encender_nodo(job['n'])
+        time.sleep(job['t'])
+        apagar_nodo(job['n'])
+        time.sleep(job['d'])
+    
+    mongo.db.jobs.update_one({"_id": job["_id"]}, {"$set": {"a": 0}})
 
 @app.route('/job', methods=['GET'])
 def get_jobs():
@@ -54,7 +81,7 @@ def get_jobs():
 def create_job():
     data = request.json
     try:
-        job = Job(data['n'], data['nm'], data['t'], data['d'])
+        job = Job(data['n'], data['nm'], data['t'], data['d'], 0)
         job_id = mongo.db.jobs.insert_one(vars(job)).inserted_id
     except KeyError as e:
         return {'error': f'Falta el campo {e}'}, 400
@@ -69,6 +96,27 @@ def delete_job(job_id):
     if result.deleted_count == 0:
         return {'error': 'Job no encontrado'}, 404
     return {'message': 'Job eliminado exitosamente'}, 200
+
+@app.route('/job/run/<job_id>', methods=['POST'])
+def start_job(job_id):
+    job = mongo.db.jobs.find_one({'_id': ObjectId(job_id)})
+    if not job:
+        return {'error': 'Job no encontrado'}, 404
+    
+    
+    mongo.db.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": {"a": 1}})
+    executor.submit(run_job, job)
+    return {'message': 'Job iniciado exitosamente'}, 200
+
+@app.route('/job/stop/<job_id>', methods=['POST'])
+def stop_job(job_id):
+    job = mongo.db.jobs.find_one({'_id': ObjectId(job_id)})
+    if not job:
+        return {'error': 'Job no encontrado'}, 404
+    
+    mongo.db.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": {"a": 0}})
+    return {'message': 'Job detenido exitosamente'}, 200
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
